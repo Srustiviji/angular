0bf193e8eb8dd0b61e63adf58011ba186e081362
srusti <vijayalakshmi@sparkouttech.com>
2022-05-19 18:00:54 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { LoginStudentComponent } from './components/login-student/login-student.component';
+import { RegisterStudentComponent } from './components/register-student/register-student.component';
+import { ListStudentComponent } from './components/list-student/list-student.component';
+const routes: Routes = [
+  {
+    path: 'add',
+    component: 'AddStudentComponent'
+  }
+  {
+    path: 'edit',
+    component: 'EditStudentComponent'
+  }
+  {
+    path: 'login',
+    component: 'LoginStudentComponent'
+  }
+  {
+    path: 'register',
+    component: 'RegisterStudentComponent'
+  }
+  {
+    path: 'list',
+    component: 'ListStudentComponent'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
